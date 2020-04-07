588dea2a0cb174a775209173b04859a7c8c3039d
rsnazario <rafael.s.nazario@gmail.com>
2020-04-07 18:32:10 -0300

    Added main functionality for Bot

--- lib/feedback_bot/commands/feelings.rb
+++ lib/feedback_bot/commands/feelings.rb
@@ -1,1 +1,21 @@
 require_relative '../classes/answers.rb'
+
+require 'open-uri'
+
+module FeedBack
+  module Commands
+    class Feelings < SlackRubyBot::Commands::Base
+      command 'feel_great' do |client, data, _match|
+        client.say(channel: data.channel, text: Answers.great)
+      end
+
+      command 'feel_meh' do |client, data, _match|
+        client.say(channel: data.channel, text: Answers.meh)
+      end
+
+      command 'feel_sad' do |client, data, _match|
+        client.say(channel: data.channel, text: Answers.sad)
+      end
+    end
+  end
+end
