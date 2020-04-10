2f404b2c19b570bebe7b4b5ffb484f33a2945b5b
rsnazario <rafael.s.nazario@gmail.com>
2020-04-10 12:36:20 -0300

    Added rspec tests

--- spec/spec_bot.rb
+++ spec/spec_bot.rb
@@ -3,7 +3,8 @@ require './lib/cnn_news_bot/bot'
 
 describe CnnNews::Bot do
   let(:object) { CnnNews::Bot.instance }
-  context 'not configured' do
+  
+  context 'slack_token not configured' do
     before do
       @slack_api_token = ENV.delete('SLACK_API_TOKEN')
     end
@@ -15,7 +16,7 @@ describe CnnNews::Bot do
     end
   end
 
-  context 'configured' do
+  context 'slack_token configured' do
     @slack_api_token = ENV['SLACK_API_TOKEN']
     it 'Requires SLACK_API_TOKEN' do
       expect { object }.not_to raise_error
